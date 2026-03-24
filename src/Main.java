4b335c3a0e4a05ad584d63a78d66cef09e6a53c0
Nills Maillet <mailletnills@gmail.com>
2026-03-24 14:46:07 +0100

    done: Exercice 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    // Exercice 1
+    Scanner scn = new Scanner(System.in);
 
+    System.out.println("Entrez votre nom : ");
+    String nom = scn.nextLine();
+    System.out.println("Entrez votre prénom : ");
+    String prenom = scn.nextLine();
+    System.out.println("Entrez votre année de naissance : ");
+    int anneeDeNaissance = scn.nextInt();
+    int age = 2026 - anneeDeNaissance;
+
+    System.out.println(prenom + " " + nom + " a " + age +" ans.");
 }
